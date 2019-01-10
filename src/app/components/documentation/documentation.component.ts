cccabbf2f8f4f08416f1b514f35fa6ea9da21c69
r.marques <r.marques@betclicgroup.com>
2019-01-10 17:14:08 +0100

    Improvements, Adding forkJoin

--- src/app/components/documentation/documentation.component.ts
+++ src/app/components/documentation/documentation.component.ts
@@ -7,4 +7,5 @@ import { Component, Input } from '@angular/core';
 })
 export class DocumentationComponent {
   @Input() public link: string;
+  @Input() public marble: string;
 }
--- src/app/components/operators/fork-join/fork-join.component.ts
+++ src/app/components/operators/fork-join/fork-join.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {BasePageComponent} from '../../base-page/base-page.component';
+import {IPageComponent} from '../../../interfaces/page-component.interface';
+import {forkJoin, interval} from 'rxjs';
+import {take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-fork-join',
   templateUrl: './fork-join.component.html',
   styleUrls: ['./fork-join.component.scss']
 })
-export class ForkJoinComponent implements OnInit {
+export class ForkJoinComponent extends BasePageComponent implements IPageComponent {
+  public start(): void {
+    super.start();
 
-  constructor() { }
+    const observable = forkJoin(
+      interval(1000).pipe(take(3)), // emit 0, 1, 2 every second and complete
+      interval(500).pipe(take(4)),  // emit 0, 1, 2, 3 every half a second and complete
+    );
 
-  ngOnInit() {
+    this.plug(observable);
   }
-
 }
