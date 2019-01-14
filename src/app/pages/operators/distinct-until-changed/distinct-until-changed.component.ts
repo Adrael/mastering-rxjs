8d6e4ce99b659736ea60d1f690644557d5c44765
r.marques <r.marques@betclicgroup.com>
2019-01-14 17:41:46 +0100

    Adding distinctUntilChanged

--- src/app/pages/operators/distinct-until-changed/distinct-until-changed.component.ts
+++ src/app/pages/operators/distinct-until-changed/distinct-until-changed.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {BasePageComponent} from '../../../components/base-page/base-page.component';
+import {IPageComponent} from '../../../interfaces/page-component.interface';
+import {of} from 'rxjs';
+import {distinctUntilChanged} from 'rxjs/operators';
 
 @Component({
   selector: 'app-distinct-until-changed',
   templateUrl: './distinct-until-changed.component.html',
   styleUrls: ['./distinct-until-changed.component.scss']
 })
-export class DistinctUntilChangedComponent implements OnInit {
+export class DistinctUntilChangedComponent extends BasePageComponent implements IPageComponent {
+  public start(): void {
+    super.start();
 
-  constructor() { }
+    interface Person {
+      age: number;
+      name: string;
+    }
 
-  ngOnInit() {
+    const observable = of<Person>(
+      {age: 4, name: 'Foo'},
+      {age: 7, name: 'Bar'},
+      {age: 5, name: 'Foo'},
+      {age: 6, name: 'Foo'},
+    ).pipe(
+      distinctUntilChanged((a: Person, b: Person) => a.name === b.name),
+    );
+
+    this.plug(observable);
   }
-
 }
