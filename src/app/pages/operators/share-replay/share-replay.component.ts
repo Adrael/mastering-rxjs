baa57732599dcbcc0f4423542cdf0e236fadd0dc
r.marques <r.marques@betclicgroup.com>
2019-01-15 18:35:23 +0100

    Adding shareReplay

--- src/app/pages/operators/share-replay/share-replay.component.ts
+++ src/app/pages/operators/share-replay/share-replay.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {BasePageComponent} from '../../../components/base-page/base-page.component';
+import {IPageComponent} from '../../../interfaces/page-component.interface';
+import {interval} from 'rxjs';
+import {shareReplay, take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-share-replay',
   templateUrl: './share-replay.component.html',
   styleUrls: ['./share-replay.component.scss']
 })
-export class ShareReplayComponent implements OnInit {
+export class ShareReplayComponent extends BasePageComponent implements IPageComponent {
+  public start(): void {
+    super.start();
 
-  constructor() { }
+    const observable = interval(1000)
+      .pipe(
+        take(4),
+        shareReplay(3)
+      );
 
-  ngOnInit() {
+    this.plug(observable, 'A');
+    this.plug(observable, 'B');
   }
-
 }
